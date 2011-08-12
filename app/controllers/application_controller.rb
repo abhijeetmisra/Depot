e7b55078c34b82fa5fc52eb17c20f2875bef6f60
Abhijeet Misra <abhijeet.misra@gmail.com>
2011-08-13 00:23:52 +0530

    Cart Creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-  @current_time = Time.now
   protect_from_forgery
+  private
+  
+  def current_cart 
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound 
+    cart = Cart.create 
+    session[:cart_id] = cart.id 
+    cart
+  end
 end
