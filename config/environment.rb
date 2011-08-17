2bdacb1f9712355d57cb7d989a51249ed6d1d343
Abhijeet Misra <abhijeet.misra@gmail.com>
2011-08-17 22:14:03 +0530

    Check Out

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,21 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address  =>  "smtp.gmail.com",
+    :port     =>  587,
+    :domain   =>  "",
+    :authentication =>  "plain",
+    :user_name  => "railsbarons@gmail.com",
+    :password =>  "workatror83",
+    :enable_starttls_auto => true
+  }
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Depot::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get "store/index"
+  
+  resources :products do 
+    get :who_bought, :on => :member
+  end
 
   resources :products
 
